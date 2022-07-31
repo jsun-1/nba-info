4c035be7c27032f84456229ce572c34eea33d755
Jared Sun <jaredsun1@gmail.com>
2022-07-30 20:59:04 -0700

    implement first template views

--- nba_info/players/views.py
+++ nba_info/players/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.template import loader
+from .models import Player
 
 
 def index(request):
-    return HttpResponse('Player index placeholder')
+    players = Player.objects.all()
+    context = {
+        'players': players
+    }
+
+    template = loader.get_template('players/index.html')
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, player_id):
+    try:
+        player = Player.objects.get(pk=player_id)
+    except Player.DoesNotExist:
+        raise Http404(f'Player {player_id} does not exist.')
+
+    context = {
+        'player': player
+    }
+    template = loader.get_template('players/detail.html')
+    return HttpResponse(template.render(context, request))
