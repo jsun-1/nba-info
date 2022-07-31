74c5aecfc2558395f91e2ed1b60f1d0d9b21c947
Jared Sun <jaredsun1@gmail.com>
2022-07-31 10:24:28 -0700

    created basic linked views for teams and players

--- nba_info/players/views.py
+++ nba_info/players/views.py
@@ -1,26 +1,15 @@
 from django.http import Http404, HttpResponse
-from django.template import loader
+from django.views import generic
+
 from .models import Player
 
 
-def index(request):
-    players = Player.objects.all()
-    context = {
-        'players': players
-    }
-
-    template = loader.get_template('players/index.html')
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    model = Player
+    template_name = 'players/index.html'
+    context_object_name = 'players'
 
 
-def detail(request, player_id):
-    try:
-        player = Player.objects.get(pk=player_id)
-    except Player.DoesNotExist:
-        raise Http404(f'Player {player_id} does not exist.')
-
-    context = {
-        'player': player
-    }
-    template = loader.get_template('players/detail.html')
-    return HttpResponse(template.render(context, request))
+class DetailView(generic.DetailView):
+    model = Player
+    template_name = 'players/detail.html'
--- nba_info/teams/views.py
+++ nba_info/teams/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.views import generic
+
+from .models import Team
 
 
-def index(request):
-    return HttpResponse('Team index placeholder')
+class IndexView(generic.ListView):
+    model = Team
+    template_name = 'teams/index.html'
+    context_object_name = 'teams'
+
+    def get_queryset(self):
+        print('test')
+        results = Team.objects.all()
+        print(results)
+        return results
+
+
+class DetailView(generic.DetailView):
+    model = Team
+    template_name = 'teams/detail.html'
