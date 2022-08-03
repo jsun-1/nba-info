c50cfca53d7283c41fcd54e9b32d579b1e9d7a9b
Jared Sun <jaredsun1@gmail.com>
2022-08-02 20:25:14 -0700

    added sorting by player contract salary

--- nba_info/teams/models.py
+++ nba_info/teams/models.py
@@ -6,3 +6,9 @@ class Team(models.Model):
 
     def __str__(self):
         return self.name
+
+    def top_players_by_salary(self):
+        """Return a list of players sorted by their maximum salary."""
+        return self.players\
+            .annotate(max_salary=models.Max('contract__salaries__salary'))\
+            .order_by('-max_salary')
